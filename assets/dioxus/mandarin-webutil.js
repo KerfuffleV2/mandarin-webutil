d5a733a25337ca8410b9a675014d8bc83bcda59b
KerfuffleV2 <kerfliffle@keemail.me>
2022-07-29 07:24:57 -0600

    Deploy pages

--- assets/dioxus/mandarin-webutil.js
+++ assets/dioxus/mandarin-webutil.js
@@ -981,15 +981,15 @@ imports.wbg.__wbg_instanceof_Window_42f092928baaee84 = function() { return logEr
     _assertBoolean(ret);
     return ret;
 }, arguments) };
-imports.wbg.__wbindgen_closure_wrapper638 = function() { return logError(function (arg0, arg1, arg2) {
+imports.wbg.__wbindgen_closure_wrapper636 = function() { return logError(function (arg0, arg1, arg2) {
     const ret = makeMutClosure(arg0, arg1, 39, __wbg_adapter_18);
     return addHeapObject(ret);
 }, arguments) };
-imports.wbg.__wbindgen_closure_wrapper699 = function() { return logError(function (arg0, arg1, arg2) {
+imports.wbg.__wbindgen_closure_wrapper697 = function() { return logError(function (arg0, arg1, arg2) {
     const ret = makeClosure(arg0, arg1, 39, __wbg_adapter_21);
     return addHeapObject(ret);
 }, arguments) };
-imports.wbg.__wbindgen_closure_wrapper723 = function() { return logError(function (arg0, arg1, arg2) {
+imports.wbg.__wbindgen_closure_wrapper721 = function() { return logError(function (arg0, arg1, arg2) {
     const ret = makeMutClosure(arg0, arg1, 39, __wbg_adapter_24);
     return addHeapObject(ret);
 }, arguments) };
